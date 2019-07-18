c587554e0336d17f54ee27da5f0704563d1ea5b8
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2019-07-18 15:01:04 +0530

    Update bisection.c
    update

--- bisection.c
+++ bisection.c
@@ -1,32 +1,20 @@
 #include<stdio.h>
 #include<math.h>
 #include<conio.h>
+#include<ctype.h>
+#include<string.h>
 
-float func(int x, int y, int z) {
-
-
-	if (f0 > 0 && f1 > 0) {} 
-
-	if (f0 > 0 && f2 > 0) {}
-
-
-	if (f0 < 0 && f1 < 0) {}
-
-
-	if (f0 < 0 && f2 < 0) {}
-
-
-}
 
 
 float main() {
 
 	float f1 = 0;
 	float f2 = 0;
-	float f0 = 0; 
+	float f0 = 0;
 	float x1 = 0;
 	float x2 = 0;
-	float x0 = 0; 
+	float x0 = 0;
+	float save = -0.000002;
 
 	printf("\nit no.\t");
 	printf("x1\t");
@@ -38,17 +26,18 @@ float main() {
 
 
 
-	for (int j = 0; j < 1; j++) {
+
 
 
 		for (int i = -15; i <= 15; i++) {
-			f1 = i*i - 3 * (i)-8;
-			f2 = (i + 1)*(i + 1) - 8 * (i + 1) - 8;
+			f1 = i*i - 4 * (i)-10;
+			f2 = (i + 1)*(i + 1) - 4 * (i + 1) - 10;
 
 			if ((f1 > 0 && f2 < 0) || (f1 < 0 && f2 > 0)) {
 
-				x1 = (float)i;
+				x1 = floor(((float)i)*1000)/1000;
 				x2 = (float)i + 1;
+			
 
 				break;
 
@@ -60,31 +49,45 @@ float main() {
 		}
 
 
-
-
+		for (int j = 0; j < 100; j++) {
+			printf("\n"); 
 		printf("%d\t", j + 1);
-		printf("%.3f\t", x1);
+		printf("%f\t", x1);
 		printf("%.3f\t", x2);
 		float x0 = ((x1 + x2) / 2);
+		f1 = x1*x1 - 4 * (x1)-10;
+		f2 = (x1 + 1)*(x1 + 1) - 4 * (x1 + 1) - 10;
+		
+		
+
 		printf("%.3f\t\t", x0);
 		printf("%.3f\t", f1);
 		printf("%.3f\t", f2);
-		f0 = x0*x0 - 3 * x0 - 8;
+		f0 = x0*x0 - 4 * x0 - 10;
 		printf("%.3f\t", f0);
+		if (save == x0) {
+
+			break;
+		}
+		save = x0; 
 
 
+		if (f0 > 0 && f1 > 0) {
+			x1 = x0; 
+		}
 
+		if (f0 > 0 && f2 > 0) {
+			x2 = x0;
+		}
 
+		if (f0 < 0 && f1 < 0) {
+			x1 = x0;
+		}
 
-
-
-
-
-
-
-
-
-
+		if (f0 < 0 && f2 < 0) {
+			x2 = x0;
+		}
+			
 	}
 	_getch();
 
