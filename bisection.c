85e62e703094519ea41cdfc77c70b9e86b11d17c
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2019-07-24 14:35:56 +0530

    Update bisection.c

--- bisection.c
+++ bisection.c
@@ -5,23 +5,13 @@
 #include<conio.h>
 
 
-/* Numerical Method - Sem 3 - Bisection Method */ 
-
-
-float pres (float o) {
-printf("%f", o);
-float mu; 
-char str[100];
-sprintf(str, "%.4f", o) ; 
-sscanf(str, "%f" ,&mu); 
-printf("%f", mu); 
-
-}
+/* Numerical Method - Sem 3 - Bisection Method */
 
 
 
 
-float main() {
+
+int main() {
 	float f1 = 0; // f(x1)
 	float f2 = 0; // f(x2)
 	float f0 = 0; //f(x3)
@@ -29,8 +19,8 @@ float main() {
 	float x2 = 0;
 	float x0 = 0;
 	float save = -0.000002;
-	int i, j ; 
-	
+	int i, j;
+
 	printf("\nit no.\t");
 	printf("x1\t");
 	printf("x2\t");
@@ -39,48 +29,62 @@ float main() {
 	printf("f(x2)\t");
 	printf("f(x0)\t\n");
 
-		for (i = -15; i <= 15; i++) {
-			f1 = i*i - 4 * (i)-10;
-			f2 = (i + 1)*(i + 1) - 4 * (i + 1) - 10;
+	for (i = -15; i <= 15; i++) {
+		f1 = i*i - 4 * (i)-10;
+		f2 = (i + 1)*(i + 1) - 4 * (i + 1) - 10;
 
-			if ((f1 > 0 && f2 < 0) || (f1 < 0 && f2 > 0)) {
+		if ((f1 > 0 && f2 < 0) || (f1 < 0 && f2 > 0)) {
 
-				x1 =  pres((float)i)  ;
-				x2 = (float)i + 1;
-			
-				break;
+			x1 = (float)i;
+			float mu;
+			char str[100];
+			sprintf(str, "%.4f", x1);
+			sscanf(str, "%f", &mu);
+			x1 = mu;
 
-			}
-
+			x2 = (float)i + 1;
+			sprintf(str, "%.4f", x2);
+			sscanf(str, "%f", &mu);
+			x0 = mu;
+			break;
 
 		}
+	}
 
 
-		for ( j = 0; j < 100; j++) {
-			printf("\n"); 
+	for (j = 0; j < 100; j++) {
+		printf("\n");
 		printf("%d\t", j + 1);
-		printf("%f\t", x1);
+		printf("%.4f\t", x1);
 		printf("%.4f\t", x2);
 		float x0 = ((x1 + x2) / 2);
+
+		float mu;
+		char str[100];
+		sprintf(str, "%.3f", x0);
+		sscanf(str, "%f", &mu);
+		x0 = mu;
+
 		f1 = x1*x1 - 4 * (x1)-10;
 		f2 = (x1 + 1)*(x1 + 1) - 4 * (x1 + 1) - 10;
-		
-		
+
+
 
 		printf("%.4f\t\t", x0);
 		printf("%.4f\t", f1);
 		printf("%.4f\t", f2);
 		f0 = x0*x0 - 4 * x0 - 10;
 		printf("%.4f\t", f0);
+
 		if (save == x0) {
 
 			break;
 		}
-		save = x0; 
+		save = x0;
 
 
 		if (f0 > 0 && f1 > 0) {
-			x1 = x0; 
+			x1 = x0;
 		}
 
 		if (f0 > 0 && f2 > 0) {
@@ -94,7 +98,7 @@ float main() {
 		if (f0 < 0 && f2 < 0) {
 			x2 = x0;
 		}
-			
+
 	}
 	_getch();
 
