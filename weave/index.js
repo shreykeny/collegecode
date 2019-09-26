c1b6c71c1b924d8ef036e0ab3c3db13cbf6edfb0
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2019-09-26 16:47:50 +0530

    Add files via upload

---
+++ weave/index.js
@@ -0,0 +1,49 @@
+var about = document.getElementById("about");
+
+var register = document.getElementById("register");
+var faq = document.getElementById("faq");
+
+
+
+
+about.addEventListener("mouseover", function() {
+
+    about.style.fontSize = "38px";
+
+})
+
+about.addEventListener("mouseout", function() {
+
+    about.style.fontSize = "";
+
+
+})
+
+home.addEventListener("mouseover", function() {
+   home.style.width = "38px";
+})
+
+home.addEventListener("mouseout", function() {
+    home.style.width = "";
+
+})
+
+register.addEventListener("mouseover", function() {
+    register.style.fontSize = "38px";
+
+ })
+ 
+register.addEventListener("mouseout", function() {
+    register.style.fontSize = "";
+
+ })
+
+faq.addEventListener("mouseover", function() {
+    faq.style.fontSize = "38px";
+
+ })
+ 
+faq.addEventListener("mouseout", function() {
+    faq.style.fontSize = "";
+
+ })
